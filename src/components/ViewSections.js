cc0fd6a0b0722fcb642b4cae582f9f7fb50846b6
unknown <elhamhashemi56@yahoo.com>
2023-04-08 16:49:00 +0200

    create ViewSections Component

---
+++ src/components/ViewSections.js
@@ -0,0 +1,9 @@
+const ViewSections=({sectionData,sectionIndex})=>{
+    return(
+        <div className="ml-5" >
+            <p>{sectionData.sectionName}</p>
+        </div>
+    )
+}
+
+export default ViewSections
--- src/components/page/Inbox.js
+++ src/components/page/Inbox.js
@@ -1,86 +1,16 @@
-import { useState } from "react"
-import { Button,Form } from 'react-bootstrap';
+
 import "./inbox.css"
+import AddSection from "../AddSection"
+import ViewSections from "../ViewSections.js"
 
 
 const Inbox=()=>{
-    const [showForm, setShowForm] = useState(false);
-    const toggleForm = () => setShowForm(!showForm);
-    const [section,setSection]=useState([
-        {
-            sectionName:"",
-            task:[
-                {
-                    taskName:"",
-                    taskDescription:""
-
-                }
-            ]
-        }
-    ])
-
-    const handleChangeSection=(e) => {
-        const updatedSections = [...section];
-        updatedSections[section.length - 1].sectionName = e.target.value;
-        setSection(updatedSections);
-    }
-
-    const handleAddSection = () => {
-        
-        const newSection = {
-            sectionName: "",
-            task: [
-                {
-                    taskName: "",
-                    taskDescription: ""
-                }
-            ]
-        }
-        setSection([...section, newSection]);
-    }
-
-    const handleCancel =()=>{
-        const updateSection=[...section]
-        updateSection[section.length-1].sectionName=""
-        setSection(updateSection)
-    }
-
-    return (
-        <div className="ml-5">
-            {!showForm && 
-            <div className="section_container"> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="board_add_section_button__icon mr-3"><path fill="currentColor" d="M19.5 20a.5.5 0 010 1h-15a.5.5 0 010-1h15zM18 6a2 2 0 012 2v8a2 2 0 01-2 2H6a2 2 0 01-2-2V8a2 2 0 012-2h12zm0 1H6a1 1 0 00-1 1v8a1 1 0 001 1h12a1 1 0 001-1V8a1 1 0 00-1-1zm-6 2a.5.5 0 01.5.5v2h2a.5.5 0 010 1h-2v2a.5.5 0 01-1 0v-2h-2a.5.5 0 010-1h2v-2A.5.5 0 0112 9zm7.5-6a.5.5 0 010 1h-15a.5.5 0 010-1h15z"></path></svg>
-                <Button className="section_container" onClick={toggleForm}>Add Section</Button>
-            </div>
-           }
-            {showForm && (
-                <Form>
-                    <input type="text" 
-                           className="form-control mb-3 ml-0 sectionInput" 
-                           placeholder="Name This Section" 
-                           value={section[section.length-1].sectionName}
-                           onChange={handleChangeSection}
-                           />
-                    
-                    {/* <Button disabled={!section.sectionName} className="mr-2 addSectionButton">Add Section</Button> */}
-                    <Button
-                        disabled={!section[section.length-1].sectionName}
-                        className="mr-2 addSectionButton"
-                        onClick={handleAddSection}
-                    >
-                        Add Section
-                    </Button>
-                    <Button className="cancelButton"
-                            onClick={() => {
-                                        toggleForm();
-                                        handleCancel();
-                                    }}    
-                                
-                                    >Cancel</Button>
-                </Form>
-            )}
-        </div>
-    )
+   return(
+    <>
+        <AddSection />
+        {/* <ViewSections /> */}
+    </>
+   )
 }
 
 export default Inbox
