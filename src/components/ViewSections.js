55c675c22421ec4376777a80708c45dca06fef28
unknown <elhamhashemi56@yahoo.com>
2023-04-24 19:24:49 +0200

    add task to section

--- src/components/ViewSections.js
+++ src/components/ViewSections.js
@@ -4,25 +4,28 @@ import { useState } from "react";
 
 const ViewSections=({sectionData,sectionIndex,setSection})=>{
     const [showTaskForm, setShowTaskForm] = useState(false);
-    const [taskSection,setTaskSection]=useState([
-        {   
-            taskNameSection:"",
-            taskDescriptionSection:""
+    const [taskValue,setTaskValue]=useState(
+        {
+            taskName:"",
+            taskDescription:""
         }
+    )
+    const [todos,setTodos]=useState([
+        // {id: 1, sectionId:"", title: "item 1", done: false},
+        // {id: 2, sectionId:"", title: "item 2", done: false},
+        // {id: 3, sectionId:"", title: "item 3", done: false},
+        // {id: 4, sectionId:"", title: "item 5", done: false},
+        // {id: 5, sectionId:"", title: "item 6", done: false},
     ])
 
-    const handleChangeTaskSectionName=(e)=>{
-        const updatedTaskSection = [...taskSection];
-        updatedTaskSection[taskSection.length - 1].taskNameSection = e.target.value;
-        setTaskSection(updatedTaskSection);
+    const handleChangeTask=(e)=>{
+        const newTaskValue = {...taskValue};
+        console.log("e.target.value",e.target.value);
+        newTaskValue[e.target.name] = e.target.value;
+        setTaskValue(newTaskValue);
     }
 
-    const handleChangeTaskSectionDescription=(e)=>{
-        const updatedTaskSection = [...taskSection];
-        updatedTaskSection[taskSection.length - 1].taskDescriptionSection = e.target.value;
-        setTaskSection(updatedTaskSection);
-    }
-
+  
     const toggleForm = () => {
         setShowTaskForm(!showTaskForm);
         
@@ -34,19 +37,29 @@ const ViewSections=({sectionData,sectionIndex,setSection})=>{
 
     const handleAddTask=()=>{
 
-        const newTaskSection = {   
-            taskNameSection: "",
-            taskDescriptionSection: ""
+        const newTodo = {   
+            title: taskValue.taskName,
+            description: taskValue.taskDescription,
+            sectionId:sectionIndex
           };
-         
-          setTaskSection([...taskSection],newTaskSection)
-
+         console.log("newTodo",newTodo);
+          setTodos([...todos,newTodo])
+          setTaskValue({taskName:"",taskDescription:""})
+          toggleForm()
     }
 
     return(
-        <div className="viewSection_Container mb-3">
+        <div className="viewSection_Container mb-5">
             <div className="mb-2">
                 <span className="sectionNameKlass ">{sectionData.sectionName}</span>
+            </div>
+
+            <div>
+                {todos.map(item=>{
+                 return   <li>
+                            {item.title}
+                          </li>
+                })}
             </div>
 
             {!showTaskForm &&
@@ -63,16 +76,23 @@ const ViewSections=({sectionData,sectionIndex,setSection})=>{
                     <input type="text"
                             placeholder="Enter Task Name"
                             className="form-control mb-3 ml-0 taskInput"
-                            value={taskSection[taskSection.length-1].taskNameSection}
-                            onChange={handleChangeTaskSectionName} 
+                            value={taskValue.taskName}
+                            name={"taskName"}
+                            onChange={handleChangeTask} 
                             />
                     <input type="text"
                             placeholder="Enter Description"
                             className="form-control mb-3 ml-0 taskInput"
-                            value={taskSection[taskSection.length-1].taskDescriptionSection}
-                            onChange={handleChangeTaskSectionDescription} 
+                            value={taskValue.taskDescription}
+                            name={"taskDescription"}
+                            onChange={handleChangeTask}
+                            
                             />
-                    <Button onClick={handleAddTask}>+</Button>
+                    <Button className="classPlusButton"
+                            onClick={handleAddTask}
+                            disabled={!taskValue.taskName}
+                            >+
+                    </Button>
                     <Button onClick={handleCancel} className="cancelButton">x</Button>
                 </Form>
             )}
