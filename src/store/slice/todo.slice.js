fd052a5ca6db72026cb96112595de7a7d08b7369
unknown <elhamhashemi56@yahoo.com>
2023-05-29 19:10:38 +0200

    make store and todo.slice in slice folder

---
+++ src/store/slice/todo.slice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice=createSlice({
+    name:"todo",
+    initialState:{
+        sections:[],
+        todos:[]
+    },
+    reducers:{
+        setSections(state,action){
+            state.sections=action.payload
+        },
+        setTodos(state,action){
+            state.todos=action.payload
+        }
+    }
+})
+
+export default slice.reducer
